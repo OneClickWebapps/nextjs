b7a70236a77f143547236426429b8c973d934df5
root <root@b32b080e-6.vce.oneclickwebapps.com>
2023-05-19 09:37:19 +0000

    Updated NextJS app.

--- src/components/ui/Content/Content.tsx
+++
@@ -1,8 +0,0 @@
-export const Content = () => {
-    return (
-        <h2 className='text-2xl max-w-md mx-auto' data-testid="content">
-            A simple Next.js application with TypeScript, Tailwind CSS and testing
-            suite enabled
-        </h2>
-    )
-}
